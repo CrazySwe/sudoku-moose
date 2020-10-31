51781e3e710c0bad4ba94cc3c1252d959a409d8c
Kevin C <crazy_swe_@hotmail.com>
2020-10-31 22:44:18 +0100

    Added comments in board.js

--- src/board.js
+++ src/board.js
@@ -7,18 +7,42 @@
 
 'use strict'
 
+/**
+ * Exports.
+ */
 module.exports.Board = Board
 module.exports.BoardParser = BoardParser
 
+/**
+ * Creates a Board instance that represents a Sudoku Board.
+ *
+ * @param {Array} board - Array representing the Sudoku Board
+ */
 function Board (board = Array(81).fill(0)) {
+  /**
+   * The Array representing the Board.
+   *
+   * @type {Array}
+   */
   this.board = board
 
+  /**
+   * Creates a string representing the board.
+   *
+   * @param {string} separator - Separator to add between digits of the Board.
+   * @returns {string} - String representing the Board.
+   */
   this.toString = function (separator = '') {
     return this.board.reduce((acc, val, index) => {
       return (index === 0) ? (acc += val) : (acc += separator + val)
     }, '')
   }
 
+  /**
+   * Creates a string formatted to be shown in the terminal.
+   *
+   * @returns {string} - Returns a nicely formatted string of the Board for the terminal.
+   */
   this.render = function () {
     return this.board.reduce((acc, val, index) => {
       return (acc += `[${val}]` + (((index + 1) % 9 === 0) ? '\n' : ''))
@@ -26,6 +50,14 @@ function Board (board = Array(81).fill(0)) {
   }
 }
 
+/**
+ * Parses a string to a Board object.
+ *
+ * @param {string} boardString - The string to parse.
+ * @param {string} emptyChar - The character that represents an empty field.
+ * @param {string} separator - If the string has custom separator between digits.
+ * @returns {Board} - The resulting board object.
+ */
 function BoardParser (boardString, emptyChar = '0', separator = '') {
   const parsedString = boardString.replace(/(\r\n|\n|\r|,)/gm, '')
   if (parsedString.length < 81) {
@@ -38,5 +70,4 @@ function BoardParser (boardString, emptyChar = '0', separator = '') {
     return Number.parseInt(char)
   })
   return new Board(board)
-  // return new this.Board(board)
 }
