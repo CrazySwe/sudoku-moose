0009aa4edb8a6f694b8f15397ec7035f51fbeab3
Kevin C <crazy_swe_@hotmail.com>
2020-10-31 22:20:36 +0100

    Added BoardParser

--- src/app.js
+++ src/app.js
@@ -9,10 +9,24 @@
 
 'use strict'
 
-const Board = require('./board')
+const { Board, BoardParser } = require('./board')
 
-const sudokuBoard = new Board()
+const existingBoardString = '000000004000000030000500001000000000000500000000000500000080000090000000000000000'
+const multiString = `
+5,3,0,0,7,0,0,0,0
+6,0,0,1,9,5,0,0,0
+0,9,8,0,0,0,0,6,0
+8,0,0,0,6,0,0,0,3
+4,0,0,8,0,3,0,0,1
+7,0,0,0,2,0,0,0,6
+0,6,0,0,0,0,2,8,0
+0,0,0,4,1,9,0,0,5
+0,0,0,0,8,0,0,7,9`
 
-console.log(sudokuBoard.toString(','))
-console.log(sudokuBoard.toString())
-console.log(sudokuBoard.render())
+const emptyBoard = new Board()
+const determinedBoard = new Board(existingBoardString.split(''))
+
+console.log(BoardParser(existingBoardString).render())
+console.log(BoardParser(multiString).render())
+console.log(emptyBoard.render())
+console.log(determinedBoard.render())
--- src/board.js
+++ src/board.js
@@ -7,7 +7,10 @@
 
 'use strict'
 
-module.exports.Board = function Board (board = Array(81).fill(0)) {
+module.exports.Board = Board
+module.exports.BoardParser = BoardParser
+
+function Board (board = Array(81).fill(0)) {
   this.board = board
 
   this.toString = function (separator = '') {
@@ -21,4 +24,19 @@ module.exports.Board = function Board (board = Array(81).fill(0)) {
       return (acc += `[${val}]` + (((index + 1) % 9 === 0) ? '\n' : ''))
     }, '')
   }
+}
+
+function BoardParser (boardString, emptyChar = '0', separator = '') {
+  const parsedString = boardString.replace(/(\r\n|\n|\r|,)/gm, '')
+  if (parsedString.length < 81) {
+    throw new Error('Expected amount of 81 digits.')
+  }
+  const board = parsedString.split(separator).map(char => {
+    if (emptyChar === char) {
+      return 0
+    }
+    return Number.parseInt(char)
+  })
+  return new Board(board)
+  // return new this.Board(board)
 }
