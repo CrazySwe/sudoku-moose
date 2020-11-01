583d68dee4c03ca4d627c96353720a5a765edc74
Kevin C <crazy_swe_@hotmail.com>
2020-11-01 10:33:38 +0100

    Added BacktrackSolver

--- src/app.js
+++ src/app.js
@@ -10,8 +10,9 @@
 'use strict'
 
 const { Board, BoardParser } = require('./board')
+const { BacktrackSolver } = require('./solver')
 
-const existingBoardString = '000000004000000030000500001000000000000500000000000500000080000090000000000000000'
+// const unsolved = '306508400520000000087000031003010080900863005050090600130000250000000074005206300'
 const multiString = `
 5,3,0,0,7,0,0,0,0
 6,0,0,1,9,5,0,0,0
@@ -23,10 +24,9 @@ const multiString = `
 0,0,0,4,1,9,0,0,5
 0,0,0,0,8,0,0,7,9`
 
-const emptyBoard = new Board()
-const determinedBoard = new Board(existingBoardString.split(''))
+const testBoard = BoardParser(multiString)
+console.log('Unsolved Board:\n' + testBoard.render())
+const solvingBoard = new BacktrackSolver(testBoard.board)
 
-console.log(BoardParser(existingBoardString).render())
-console.log(BoardParser(multiString).render())
-console.log(emptyBoard.render())
-console.log(determinedBoard.render())
+solvingBoard.solveBoard(0, 0)
+console.log('Solved Board:\n' + new Board(solvingBoard.board).render())
--- src/solver.js
+++ src/solver.js
@@ -7,6 +7,63 @@
 
 'use strict'
 
-module.exports = function Solver () {
+module.exports.BacktrackSolver = BacktrackSolver
 
+function BacktrackSolver (boardArray) {
+  this.board = boardArray.slice(0)
+  this.solveBoard = function (row = 0, col = 0) {
+    // Check out of bounds after last box.
+    if (row >= 8 && col >= 9) {
+      return true
+    }
+
+    // move to next row when reached row end.
+    if (col === 9) {
+      row++
+      col = 0
+    }
+
+    // Box needs to be "empty" else move to the next box
+    if (this.board[((row * 9) + col)] > 0) {
+      return this.solveBoard(row, col + 1)
+    }
+    // Try all digits between 1..9
+    for (let digit = 1; digit <= 9; digit++) {
+      if (this.isSafe(row, col, digit)) {
+        this.board[((row * 9) + col)] = digit
+        // try solve rest of board with that digit.
+        if (this.solveBoard(row, col + 1)) {
+          return true
+        }
+      }
+      // Reset if that digit did not work.
+      this.board[((row * 9) + col)] = 0
+    }
+    return false
+  }
+
+  this.isSafe = function (row, col, digit) {
+    // Check same row
+    for (let x = 0; x <= 8; x++) {
+      if (this.board[((row * 9) + x)] === digit) { return false }
+    }
+
+    // Check same column
+    for (let y = 0; y <= 8; y++) {
+      if (this.board[((y * 9) + col)] === digit) { return false }
+    }
+
+    // Check same 3x3 square
+    // Get top corner
+    const topRow = row - (row % 3)
+    const topCol = col - (col % 3)
+    for (let y = 0; y < 3; y++) {
+      for (let x = 0; x < 3; x++) {
+        if (this.board[((x + topRow) * 9) + (y + topCol)] === digit) {
+          return false
+        }
+      }
+    }
+    return true
+  }
 }
