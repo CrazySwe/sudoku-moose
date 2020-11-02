814a26436e9cc71685c5d219623270a2ac853b8e
Kevin C <crazy_swe_@hotmail.com>
2020-11-03 00:25:25 +0100

    Cleaning app.js

--- src/app.js
+++ src/app.js
@@ -12,23 +12,6 @@
 const { Board, BoardParser } = require('./board')
 const { Solver, SolveAlgorithm } = require('./solver')
 
-// const unsolved = '306508400520000000087000031003010080900863005050090600130000250000000074005206300'
-const multiString = `
-5,3,0,0,7,0,0,0,0
-6,0,0,1,9,5,0,0,0
-0,9,8,0,0,0,0,6,0
-8,0,0,0,6,0,0,0,3
-4,0,0,8,0,3,0,0,1
-7,0,0,0,2,0,0,0,6
-0,6,0,0,0,0,2,8,0
-0,0,0,4,1,9,0,0,5
-0,0,0,0,8,0,0,7,9`
+const unsolved = '530070000600195000098000060800060003400803001700020006060000280000419005000080079'
 
-const testBoard = BoardParser(multiString)
-console.log('Unsolved Board:\n' + testBoard.render())
-
-const result = Solver(testBoard, SolveAlgorithm.SIMPLE)
-console.log(result.render())
-// const solvingBoard = new BacktrackSolver(testBoard)
-// // solvingBoard.solveBoard(0, 0)
-// console.log('Solved Board:\n' + solvingBoard.render())
+console.log(unsolved)
